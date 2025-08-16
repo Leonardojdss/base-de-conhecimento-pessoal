285c166e8d222ee42ab06739d508a5a57e6664a3
Leonardo José da Silva <124636951+Leonardojdss@users.noreply.github.com>
2025-08-16 05:59:53 -0300

    Delete Exercícios_de_Lógica directory

--- Exercícios_de_Lógica/Ex_04.py
+++
@@ -1,8 +0,0 @@
-#estrutura de decisao ternaria
-
-numero = int(input('Digite um numero:'))
-resto = numero % 2
-
-saida = "par" if resto == 0 else "impar"
-
-print("seu numero e", saida)
--- Exercícios_de_Lógica/Ex_06.py
+++
@@ -1,6 +0,0 @@
-# Estrutura de repeticao for
-
-for numero in range(10):
-    print('valor atual:', numero)
-
-
